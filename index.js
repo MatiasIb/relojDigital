898ed0f6387088034c0018d3b585b827155447f3
MatiasIb <matiasrolando2003@gmail.com>
2022-10-17 02:17:37 -0300

    relojv1

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 
 function traerFecha(){
-    let numeros = "1,2,3,4,5,6,7,8,9"
+    let numeros = [1,2,3,4,5,6,7,8,9]
     fecha = new Date()
     let horas = fecha.getHours()
     let minutos = fecha.getMinutes()
     let segundos = fecha.getSeconds()
     let hora = document.getElementById('hora')
-    if(horas == parseInt(numeros) ){
+    
+    for(i in numeros){
+    if(horas == numeros[i] ){
         horas = '0' + horas.toString()
     }
-    if(minutos == parseInt(numeros)){
+    if(minutos == numeros[i]){
         minutos = '0' + minutos.toString()
     }
-
-    setInterval("location.reload()",10000);   
-    hora.innerHTML = horas + ' : ' + minutos + ' : ' + segundos
+}   
+    hora.innerText = horas + ' : ' + minutos + ' : ' + segundos
+    setTimeout("traerFecha()", 100)
 }
 traerFecha()
+
+
 
