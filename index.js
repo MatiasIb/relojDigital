56746f422f70177942049b4b257dc2ed376d040b
MatiasIb <matiasrolando2003@gmail.com>
2022-10-17 01:48:16 -0300

    relojv1

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function traerFecha(){
+    let numeros = "1,2,3,4,5,6,7,8,9"
+    fecha = new Date()
+    let horas = fecha.getHours()
+    let minutos = fecha.getMinutes()
+    let segundos = fecha.getSeconds()
+    let hora = document.getElementById('hora')
+    if(horas == parseInt(numeros) ){
+        horas = '0' + horas.toString()
+    }
+    if(minutos == parseInt(numeros)){
+        minutos = '0' + minutos.toString()
+    }
+
+    
+    hora.innerHTML = horas + ' : ' + minutos + ' : ' + segundos
+}
+traerFecha()
+
